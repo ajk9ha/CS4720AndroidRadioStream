22f53ea55b589977e248c9a34e769327d0ada161
Matt <Student@172.27.44.220>
2015-09-15 23:16:40 -0400

    Added user input functionality takes input in the box and changes the "world" in "hello world" to the name entered

--- app/src/main/java/cs4720/cs/virginia/edu/cs4720androidradiostream/MainActivity.java
+++ app/src/main/java/cs4720/cs/virginia/edu/cs4720androidradiostream/MainActivity.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
@@ -13,9 +16,14 @@ import com.google.android.gms.location.LocationServices;
 import static com.google.android.gms.location.LocationServices.API;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private Button name;
+    private EditText textField;
+    private TextView helloText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -24,6 +32,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
+        name = (Button) findViewById(R.id.button);
+        textField = (EditText) findViewById(R.id.editText);
+        helloText = (TextView) findViewById(R.id.textView2);
+
+        name.setOnClickListener(new View.OnClickListener() {
+                                    public void onClick(View v) {
+                                        helloText.setText(textField.getText());
+                                    }
+                                }
+        );
     }
 
     @Override
