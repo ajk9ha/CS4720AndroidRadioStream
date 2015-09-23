f4785ce5fdbff50d6ac4b5ef1cd1a875b9283b5f
Andrew <andrew@b23.io>
2015-09-23 19:27:49 -0400

    Added in SplashScreen Activity, starting to work on MediaPlayer.

---
+++ app/src/main/java/cs4720/cs/virginia/edu/cs4720androidradiostream/SplashScreen.java
@@ -0,0 +1,55 @@
+package cs4720.cs.virginia.edu.cs4720androidradiostream;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread t = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    SplashScreen.this.startActivity(intent);
+                    Log.d("test", "Splash Screen reached.");
+                    SplashScreen.this.finish();
+                }
+            }
+        };
+        t.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
